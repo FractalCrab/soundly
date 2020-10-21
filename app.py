b0a8c5a7cf40ce11aee059ac4cfa5b37a3079563
Dhruv <dhruv_devasthale@outlook.com>
2020-10-21 12:08:25 +0530

    add default html

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello"
+    # renders an html file and uses it for the url
+    return render_template("index.html", name="test")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
