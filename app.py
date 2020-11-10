ed2977a273e0a4621d24f0b16b1d00cdbd58bb17
Dhruv <dhruv_devasthale@outlook.com>
2020-11-10 15:55:27 +0530

    setup register api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import mysql.connector
-
+import random
 mydb = mysql.connector.connect(
     host="us-cdbr-east-02.cleardb.net",
     user=" b7c1d59815aede",
@@ -12,16 +12,30 @@ mycursor.execute(
     "use heroku_0f834e948b2d904;")
 
 
-
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return jsonify("test")
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return "Soundly API"
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+ 
+        if request.form != None:
+
+            username = request.form.get("username")
+            password = request.form.get("password")
+            id=random.randint(1,1000)
+            query = "insert into users values("+str(id),"user","pass"
+            mycursor.execute(query)
+            return jsonify("success")
+        else:
+            return jsonify("no data")
+        
 
 
-@app.route('/api/register', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def test():
     return jsonify(request.form.to_dict())
 
