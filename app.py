31c675598f9467c661e8a4b9e471f09074a84989
Abhinandan Sharma <40931723+FractalCrab@users.noreply.github.com>
2020-12-13 22:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,82 +41,41 @@ def login():
         print(_json)
         username = _json['username']
         password = _json['password']
+        islogged = False
         print("form "+password)
         try:
                 curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
                 curl.execute("SELECT * FROM user WHERE username=%s",(username,))
                 user = curl.fetchone()
                 # print (user)
-                curl.close()
+                # curl.close()
                 print("user"+user["password"])
                 # print(len(user))
                 print(user)
                 if (password == user["password"]):
-                    session['name'] = user['name']
-                    session['username'] = user['username']
-                    return jsonify({'message' : 'logedin success'})
+                    # session['name'] = user['name']
+                    # session['username'] = user['username']
+                    # print(session['name'])
+                    isLogged= True
+                    return "is logged"
                 else:
                         return "Error passowrd didnt match"
         except Exception as e:
                 return jsonify({'message' : 'error'})
+        finally:
+            if curl:
+                curl.close()
     else:
         return jsonify({'message' : 'please login'})
+#to be written song route
+# @app.route('/song', methods=['GET', 'POST'])
+# def index():
+  
 
-# @app.route('/login', methods=['POST'])
-# def login():
-# 	conn = None;
-# 	cursor = None;
-	
-# 	try:
-# 		_json = request.json
-# 		_username = _json['username']
-# 		_password = _json['password']
-		
-# 		# validate the received values
-# 		if _username and _password:
-# 			#check user exists			
-# 			conn =  mysql.connection
-# 			cursor = conn.cursor()
-			
-# 			sql = "SELECT * FROM user WHERE username=%s"
-# 			sql_where = (_username,)
-			
-# 			cursor.execute(sql, sql_where)
-# 			row = cursor.fetchone()
-# 			print(row[2])
-# 			# if row:
-# 			# 	if (password == user["password"]):
-# 			# 		session['username'] = row[1]
-# 			# 		#cursor.close()
-# 			# 		#conn.close()
-# 			# 		return jsonify({'message' : 'You are logged in successfully'})
-# 			# 	else:
-# 			# 		resp = jsonify({'message' : 'Bad Request - invalid password'})
-# 			# 		resp.status_code = 400
-# 			# 		return resp
-# 		else:
-# 			resp = jsonify({'message' : 'Bad Request - invalid credendtials'})
-# 			resp.status_code = 400
-# 			return resp
-
-	# except Exception as e:
-	# 	print(e)
-
-	# finally:
-	# 	if cursor and conn:
-	# 		cursor.close()
-	# 		conn.close()
-
-
-
-# def logout():
-#     session.clear()
-#     return redirect('/login')
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
-	if 'username' in session:
-		session.pop('username', None)
-	return jsonify({'message' : 'You successfully logged out'})
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(port=5001)
